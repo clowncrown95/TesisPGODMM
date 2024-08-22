1de30702e2caf1b35ead3eb784cb09531a0812b8
Jefferson Benites <pein951@hotmail.com>
2024-08-22 15:55:22 -0500

    Dto´s

---
+++ WebApiPPGODMM1/Daos/DaoDto/DaoDtoOrdenTrabajo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiPPGODMM1.Daos.DaoDto
+{
+    public class DaoDtoOrdenTrabajo
+    {
+    }
+}
---
+++ WebApiPPGODMM1/Models/DTO/DTOOrdenTrabajo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiPPGODMM1.Models.DTO
+{
+    public class DTOOrdenTrabajo
+    {
+        public int USU_USUARIO { get; set; }
+
+        public int LOC_NOMBRE { get; set; }
+
+        public string EST_NOMBRE { get; set; }
+
+        public DateTime ORD_FECHAINI { get; set; }
+
+        public DateTime ORD_FECHAFIN { get; set; }
+
+        public int ORD_NUMERO { get; set; }
+
+        public int EQU_NOMBRE { get; set; }
+
+        public string DTOR_DETALLE { get; set; }
+
+        public string RTEC_DESCRIPCION { get; set; }
+
+        public string RTEC_RESPALDO { get; set; }
+
+        public float RTEC_COSTO { get; set; }
+
+        public string RTEC_FIRMA { get; set; }
+
+    }
+
+}
+}
